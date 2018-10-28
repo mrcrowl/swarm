dbcfdeafd31302a04582010f047895887ce9b862
Ben Crowl <bcrowl@languageperfect.com>
2018-10-28 21:41:18 +1300

    Config updates

--- build.bat
+++ build.bat
@@ -1,9 +1,26 @@
 @echo off
+
+
+echo Getting go packages...
 go get
+
+echo.
+echo Building TypeScript files...
+call tsc -p ./assets/static/tsconfig.json
+
+echo ... and embedding as resources...
+resources -output assets/assets_prod.go -declare -var FS -package assets -tag=embed assets/static/*.js
+
+echo.
+echo Building 'swarm' for macOS...
 set GOARCH=amd64
 set GOOS=darwin
-echo Building Mac...
-go build -o swarm -ldflags "-s -w-x"
+go build -o swarm -ldflags "-s -w-x" -tags=embed
+
+echo.
+echo Building 'swarm.exe' for Windows...
 set GOOS=windows
-echo Building Windows...
-go build -o swarm.exe -ldflags "-s -w"
+go build -o swarm.exe -ldflags "-s -w" -tags=embed
+
+echo.
+echo Done!
