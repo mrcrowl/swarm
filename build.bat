e30611e6972689ed1600714b38b5fcc6074733bc
Ben Ben_louise Louise Crowl <ben_louise@imac.hub>
2018-10-29 22:48:12 +1300

    Separate build script for linux/mac

--- build.bat
+++ build.bat
@@ -12,14 +12,7 @@ echo ... and embedding as resources...
 resources -output assets/assets_prod.go -declare -var FS -package assets -tag=embed assets/static/*.js
 
 echo.
-echo Building 'swarm' for macOS...
-set GOARCH=amd64
-set GOOS=darwin
-go build -o swarm -ldflags "-s -w-x" -tags=embed
-
-echo.
 echo Building 'swarm.exe' for Windows...
-set GOOS=windows
 go build -o swarm.exe -ldflags "-s -w" -tags=embed
 
 echo.
